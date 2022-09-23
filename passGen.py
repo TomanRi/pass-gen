3084f29499d81c25ab8c83d4ac295eb4f472d861
TomanRi <97634536+TomanRi@users.noreply.github.com>
2022-09-23 20:14:24 +0200

    Update passGen.py

--- passGen.py
+++ passGen.py
@@ -1,5 +1,7 @@
+from cryptography import fernet
 import random
 import string
+import os
 
 print(
  "\n _____         _____ _____  _____ ______ _   _\n"
@@ -11,35 +13,48 @@ print(
  "\n-------------------------------------------------\n"                                               
 )
 
-passName = input("Note for password: ")
-print("\n" + passName + "\n-------------------------------------------------\n")
+def menu ():
+    print("[1] Create new list")
+    print("[2] Delete list")
+print()
+menu()
 
-letterLarge = list(string.ascii_uppercase)
-letterSmall = list(string.ascii_lowercase)
-number = list(string.digits)
+option = int(input("\nChoose option: "))
+while option != 0:
+    if option == 1:
+        passName = input("Note for password: ") 
+        letterLarge = list(string.ascii_uppercase)
+        letterSmall = list(string.ascii_lowercase)
+        number = list(string.digits)
 
-first1 = random.choice(letterLarge)
-first2 = random.choice(letterLarge)
-first3 = random.choice(letterLarge)
+        first1 = random.choice(letterLarge)
+        first2 = random.choice(letterLarge)
+        first3 = random.choice(letterLarge)
 
-part1 = first1 + first2 + first3
+        part1 = first1 + first2 + first3
 
-second1 = random.choice(letterSmall)
-second2 = random.choice(letterSmall)
-second3 = random.choice(letterSmall)
-second4 = random.choice(letterSmall)
+        second1 = random.choice(letterSmall)
+        second2 = random.choice(letterSmall)
+        second3 = random.choice(letterSmall)
+        second4 = random.choice(letterSmall)
 
-part2 = second1 + second2 + second3 + second4
+        part2 = second1 + second2 + second3 + second4
 
-third1 = random.choice(number)
-third2 = random.choice(number)
-third3 = random.choice(number)
-third4 = random.choice(number)
+        third1 = random.choice(number)
+        third2 = random.choice(number)
+        third3 = random.choice(number)
+        third4 = random.choice(number)
 
-part3 = third1 + third2 + third3 + third4
+        part3 = third1 + third2 + third3 + third4
 
-f = open("pass.txt", "a")
-f.write(passName + " - " + part1 + "_" + part2 + part3 + "\n")
-f.close
+        f = open("pass.txt", "a")
+        f.write(passName + " - " + part1 + "_" + part2 + part3 + "\n")
+        f.close
+    
+    elif option == 2:
+        os.remove('pass.txt')
+        
 
-#################################################################
+
+
+################################################################# MENU !!!!!!!!!!!!!!

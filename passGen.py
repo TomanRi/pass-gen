327ac939684f588f073a371638179f246710712b
TomanRi <97634536+TomanRi@users.noreply.github.com>
2022-09-23 17:24:28 +0200

    Add files via upload

---
+++ passGen.py
@@ -0,0 +1,40 @@
+
+from cryptography import fernet
+import random
+import string
+
+letterLarge = list(string.ascii_uppercase)
+letterSmall = list(string.ascii_lowercase)
+number = list(string.digits)
+
+
+
+first1 = random.choice(letterLarge)
+first2 = random.choice(letterLarge)
+first3 = random.choice(letterLarge)
+
+part1 = first1 + first2 + first3
+
+second1 = random.choice(letterSmall)
+second2 = random.choice(letterSmall)
+second3 = random.choice(letterSmall)
+second4 = random.choice(letterSmall)
+
+part2 = second1 + second2 + second3 + second4
+
+third1 = random.choice(number)
+third2 = random.choice(number)
+third3 = random.choice(number)
+third4 = random.choice(number)
+
+part3 = third1 + third2 + third3 + third4
+
+#passWord = print(part1 + "_" + part2 + part3)
+
+passName = input("Note for password: ")
+
+f = open("pass.txt", "a")
+f.write(passName + " - " + part1 + "_" + part2 + part3 + "\n")
+f.close
+
+#https://www.thepythoncode.com/article/encrypt-decrypt-files-symmetric-python

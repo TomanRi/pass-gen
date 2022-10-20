479cd5aa1c39eba5777fdf1944aacb1b3b1428b9
TomanRi <97634536+TomanRi@users.noreply.github.com>
2022-10-20 22:30:57 +0200

    Update passGen.py

--- passGen.py
+++ passGen.py
@@ -3,35 +3,26 @@ import random
 import string
 import os
 
-print(
- "\n _____         _____ _____  _____ ______ _   _\n"
- "|  __ \ /\    / ____/ ____|/ ____|  ____| \ | |\n"
- "| |__) /  \  | (___| (___ | |  __| |__  |  \| |\n"
- "|  ___/ /\ \  \___ \\___  \| | |_ |  __| | . ` |\n"
- "| |  / ____ \ ____) |___) | |__| | |____| |\  |\n"
- "|_| /_/    \_\_____/_____/ \_____|______|_| \_|\n"
- "\n-------------------------------------------------\n"                                               
-)
+option = 1
 
-def menu():
+while option != 0:
+
+    print(
+     "\n _____         _____ _____  _____ ______ _   _\n"
+     "|  __ \ /\    / ____/ ____|/ ____|  ____| \ | |\n"
+     "| |__) /  \  | (___| (___ | |  __| |__  |  \| |\n"
+     "|  ___/ /\ \  \___ \\___  \| | |_ |  __| | . ` |\n"
+     "| |  / ____ \ ____) |___) | |__| | |____| |\  |\n"
+     "|_| /_/    \_\_____/_____/ \_____|______|_| \_|\n"
+     "\n-------------------------------------------------\n"                                               
+    )
+
+    def menu():
 
         print(" [1] Password")
         print(" [2] Encryption")
         print(" [0] Exit")
 
-print()
-menu()
-
-option = int(input("\n Choose option: "))
-
-if option == 1:
-        
-    def menu():
-
-        print(" [1] Generate password")
-        print(" [2] Delete password file")
-        print(" [0] Exit")
-    
     print()
     menu()
 
@@ -39,122 +30,135 @@ if option == 1:
 
     if option == 1:
 
-        passName = input("\n Note for password: ")
-        password = ""
+        def menu():
 
-        letterLarge = list(string.ascii_uppercase)
-        letterSmall = list(string.ascii_lowercase)
-        number = list(string.digits)
+            print(" [1] Generate password")
+            print(" [2] Delete password file")
+            print(" [0] Exit")
 
-        for i in range(3):
-                password += random.choice(letterLarge)
+        print()
+        menu()
 
-        password += "_"
+        option = int(input("\n Choose option: "))
 
-        for i in range(4):
-                password += random.choice(letterSmall)
+        if option == 1:
 
-        for i in range(4):
-                password += random.choice(number)
+            passName = input("\n Note for password: ")
+            password = ""
 
-        print("\n Password: " + passName + " - " + password)
+            letterLarge = list(string.ascii_uppercase)
+            letterSmall = list(string.ascii_lowercase)
+            number = list(string.digits)
 
-        f = open("pass.txt", "a")
-        f.write(passName + " - " + password + "\n")
-        f.close
+            for i in range(3):
+                    password += random.choice(letterLarge)
 
-        input("\n Press any key to exit...")
+            password += "_"
 
-    elif option == 2:
-        
-        os.remove("pass.txt")
+            for i in range(4):
+                    password += random.choice(letterSmall)
 
-    else:
-        input("\n Press any key to exit...")
+            for i in range(4):
+                    password += random.choice(number)
 
-###########################################################
-#                       DECRYPTION                        #
-###########################################################
+            print("\n Password: " + passName + " - " + password)
 
-elif option == 2:
+            f = open("pass.txt", "a")
+            f.write(passName + " - " + password + "\n")
+            f.close
 
-    def menu():
+            input("\n Press any key to exit...")
 
-            print(" [1] Encrypt")
-            print(" [2] Decrypt")
-            print(" [3] Generate key")
-            print(" [4] Delete key")
+        elif option == 2:
 
-    print()
-    menu()
+            os.remove("pass.txt")
 
-    option = int(input("\n Choose option: "))
-    
-    if option == 1:
-
-        passList = "pass.txt"
-        
-        if os.path.exists("key.key"):
-                
-                with open("key.key", "rb") as key:
-                        key = key.read()
-
-                with open(passList, "rb") as theFile:
-                        contents = theFile.read()
-                
-                contents_encrypted = Fernet(key).encrypt(contents)
-                
-                with open(passList, "wb") as theFile:
-                        theFile.write(contents_encrypted)
-                
-                print("\n Encrypting...")
         else:
-                print("\n Key is missing!")
+            input("\n Press any key to exit...")
 
-        input("\n Press any key to exit...")
-
+    ###########################################################
+    #                       DECRYPTION                        #
+    ###########################################################
 
     elif option == 2:
 
-        print("\n Decrypting...")
+        def menu():
 
-        passList = "pass.txt"
-        
-        if os.path.exists("key.key"):
+                print(" [1] Encrypt")
+                print(" [2] Decrypt")
+                print(" [3] Generate key")
+                print(" [4] Delete key")
 
-                key = Fernet.generate_key()
+        print()
+        menu()
 
-                with open("key.key", "rb") as key:
-                        decryptKey = key.read()
+        option = int(input("\n Choose option: "))
 
-                with open(passList, "rb") as theFile:
-                        contents = theFile.read()
-                
-                contents_decrypted = Fernet(decryptKey).decrypt(contents)
-                
-                with open(passList, "wb") as theFile:
-                        theFile.write(contents_decrypted)
-        
+        if option == 1:
+
+            passList = "pass.txt"
+
+            if os.path.exists("key.key"):
+
+                    with open("key.key", "rb") as key:
+                            key = key.read()
+
+                    with open(passList, "rb") as theFile:
+                            contents = theFile.read()
+
+                    contents_encrypted = Fernet(key).encrypt(contents)
+
+                    with open(passList, "wb") as theFile:
+                            theFile.write(contents_encrypted)
+
+                    print("\n Encrypting...")
+            else:
+                    print("\n Key is missing!")
+
+            input("\n Press any key to exit...")
+
+
+        elif option == 2:
+
+            print("\n Decrypting...")
+
+            passList = "pass.txt"
+
+            if os.path.exists("key.key"):
+
+                    key = Fernet.generate_key()
+
+                    with open("key.key", "rb") as key:
+                            decryptKey = key.read()
+
+                    with open(passList, "rb") as theFile:
+                            contents = theFile.read()
+
+                    contents_decrypted = Fernet(decryptKey).decrypt(contents)
+
+                    with open(passList, "wb") as theFile:
+                            theFile.write(contents_decrypted)
+
+            else:
+                    print("\n Key is missing!")
+
+            input("\n Press any key to exit...")
+
+        elif option == 3:
+
+            key = Fernet.generate_key()
+
+            with open("key.key", "wb") as theKey:
+                            theKey.write(key)
+
+            print("\n Generating key... \n")
+
+        elif option == 4:
+
+            os.remove("key.key")
+
         else:
-                print("\n Key is missing!")
-
-        input("\n Press any key to exit...")
-
-    elif option == 3:
-        
-        key = Fernet.generate_key()
-        
-        with open("key.key", "wb") as theKey:
-                        theKey.write(key)
-
-        print("\n Generating key... \n")
-
-    elif option == 4:
-        
-        os.remove("key.key")
+            input("\n Press any key to exit...")
 
     else:
         input("\n Press any key to exit...")
-
-else:
-    input("\n Press any key to exit...")
